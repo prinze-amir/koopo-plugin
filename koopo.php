6c30bc6a423122aa0628c4c8f52ff9122c421c83
Prinze Amir <myprinzeamir@gmail.com>
2026-02-14 19:39:03 -0500

    upgrade subscription flow.

--- koopo.php
+++ koopo.php
@@ -3,7 +3,7 @@
  * Plugin Name: Koopo
  * Plugin URI: http://www.docs.koopoonline.com/
  * Description: Custom blocks and shortcodes for advance features.
- * Version: 2.29
+ * Version: 2.295
  * Author: Plu2oprinze
  * Author URI: http://www.koopoonline.com
  */
